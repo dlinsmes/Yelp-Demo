6807b16c2e704b8aed905a75a1f6c89d5c01f426
dlinsmes <david.lin@smes.org>
2022-10-20 08:35:23 -0700

    JSON Demo

---
+++ src/JSONDemo.java
@@ -0,0 +1,71 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class JSONDemo {
+    public static void main(String [] args) throws JSONException {
+        String accessToken = null;
+        OkHttpClient client = new OkHttpClient();
+
+        String term = "Taco";                      // term
+        String location = "Irvine, CA";            // location
+        String price = "1";                        // price        1 = $, 2 = $$, 3 = $$$, 4 = $$$$
+
+
+        Request request = new Request.Builder()
+                .url("https://api.yelp.com/v3/businesses/search?term=" + term + "&location=" + location + "&sort_by=rating&price="+price+"")
+                .get()
+                .addHeader("authorization", "Bearer "+accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string().trim());       // parser
+
+            //debug to see the keys in the jsonObject: total, region, businesses
+            System.out.println("total: " + jsonObject.get("total"));
+            System.out.println("region: " + jsonObject.get("region"));
+            System.out.println("businesses: " + jsonObject.get("businesses"));
+            System.out.println();
+
+
+            //region is an object - note the prev printout was in curly braces:
+            System.out.println("region:");
+            //need to get the object stored to the key center
+            System.out.println( ((JSONObject)jsonObject.get("region")).get("center") );
+            //get latitude double value from region
+            System.out.println("latitude:");
+            System.out.println( ((JSONObject)((JSONObject)jsonObject.get("region")).get("center")).getDouble("latitude") );
+            System.out.println();
+
+
+            //businesses is an array - note the printout was contained in square brackets
+            JSONArray bArray = (JSONArray)jsonObject.get("businesses");
+            System.out.println("length of bArray: " + bArray.length());
+            System.out.println();
+
+            for (int i = 0; i < bArray.length(); i++) {
+                //each element of businesses is a JSONObject
+                System.out.println("obj " + i);
+                JSONObject obj = bArray.getJSONObject(i);
+                System.out.println("JSONObject: " + obj);
+
+                //isolate fields of each object by key:
+                System.out.println("name: " + obj.getString("name"));
+                System.out.println("distance: " + obj.getDouble("distance"));
+
+                System.out.println();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
